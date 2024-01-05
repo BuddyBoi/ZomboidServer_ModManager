fee0acaa1f601e7453aa21d467bf118809056bd5
Braxton <BuddyBoi@users.noreply.github.com>
2024-01-04 20:53:13 -0500

    Add project files.

---
+++ ZomboidServerModManager/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace ZomboidServerModManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listboxModsAvailable = new System.Windows.Forms.ListBox();
+            this.listboxModsEnabled = new System.Windows.Forms.ListBox();
+            this.buttonModEnable = new System.Windows.Forms.Button();
+            this.buttonModDisable = new System.Windows.Forms.Button();
+            this.buttonModSetupList = new System.Windows.Forms.Button();
+            this.listboxModSelectedPreview = new System.Windows.Forms.ListBox();
+            this.labelOne = new System.Windows.Forms.Label();
+            this.richtextGeneratedConfig = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // listboxModsAvailable
+            // 
+            this.listboxModsAvailable.FormattingEnabled = true;
+            this.listboxModsAvailable.Location = new System.Drawing.Point(12, 12);
+            this.listboxModsAvailable.Name = "listboxModsAvailable";
+            this.listboxModsAvailable.Size = new System.Drawing.Size(305, 238);
+            this.listboxModsAvailable.TabIndex = 0;
+            this.listboxModsAvailable.SelectedIndexChanged += new System.EventHandler(this.listboxModsAvailable_SelectedIndexChanged);
+            // 
+            // listboxModsEnabled
+            // 
+            this.listboxModsEnabled.FormattingEnabled = true;
+            this.listboxModsEnabled.Location = new System.Drawing.Point(333, 12);
+            this.listboxModsEnabled.Name = "listboxModsEnabled";
+            this.listboxModsEnabled.Size = new System.Drawing.Size(306, 238);
+            this.listboxModsEnabled.TabIndex = 1;
+            // 
+            // buttonModEnable
+            // 
+            this.buttonModEnable.Location = new System.Drawing.Point(12, 256);
+            this.buttonModEnable.Name = "buttonModEnable";
+            this.buttonModEnable.Size = new System.Drawing.Size(305, 23);
+            this.buttonModEnable.TabIndex = 2;
+            this.buttonModEnable.Text = "Add";
+            this.buttonModEnable.UseVisualStyleBackColor = true;
+            this.buttonModEnable.Click += new System.EventHandler(this.buttonModEnable_Click);
+            // 
+            // buttonModDisable
+            // 
+            this.buttonModDisable.Location = new System.Drawing.Point(334, 256);
+            this.buttonModDisable.Name = "buttonModDisable";
+            this.buttonModDisable.Size = new System.Drawing.Size(305, 23);
+            this.buttonModDisable.TabIndex = 3;
+            this.buttonModDisable.Text = "Remove";
+            this.buttonModDisable.UseVisualStyleBackColor = true;
+            this.buttonModDisable.Click += new System.EventHandler(this.buttonModDisable_Click);
+            // 
+            // buttonModSetupList
+            // 
+            this.buttonModSetupList.Location = new System.Drawing.Point(12, 379);
+            this.buttonModSetupList.Name = "buttonModSetupList";
+            this.buttonModSetupList.Size = new System.Drawing.Size(627, 23);
+            this.buttonModSetupList.TabIndex = 4;
+            this.buttonModSetupList.Text = "Generate Config";
+            this.buttonModSetupList.UseVisualStyleBackColor = true;
+            this.buttonModSetupList.Click += new System.EventHandler(this.buttonModSetupList_Click);
+            // 
+            // listboxModSelectedPreview
+            // 
+            this.listboxModSelectedPreview.FormattingEnabled = true;
+            this.listboxModSelectedPreview.Location = new System.Drawing.Point(12, 304);
+            this.listboxModSelectedPreview.Name = "listboxModSelectedPreview";
+            this.listboxModSelectedPreview.Size = new System.Drawing.Size(305, 69);
+            this.listboxModSelectedPreview.TabIndex = 5;
+            // 
+            // labelOne
+            // 
+            this.labelOne.AutoSize = true;
+            this.labelOne.Location = new System.Drawing.Point(12, 288);
+            this.labelOne.Name = "labelOne";
+            this.labelOne.Size = new System.Drawing.Size(95, 13);
+            this.labelOne.TabIndex = 6;
+            this.labelOne.Text = "Selected mod info:";
+            // 
+            // richtextGeneratedConfig
+            // 
+            this.richtextGeneratedConfig.Location = new System.Drawing.Point(12, 408);
+            this.richtextGeneratedConfig.Name = "richtextGeneratedConfig";
+            this.richtextGeneratedConfig.Size = new System.Drawing.Size(627, 170);
+            this.richtextGeneratedConfig.TabIndex = 8;
+            this.richtextGeneratedConfig.Text = "";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 590);
+            this.Controls.Add(this.richtextGeneratedConfig);
+            this.Controls.Add(this.labelOne);
+            this.Controls.Add(this.listboxModSelectedPreview);
+            this.Controls.Add(this.buttonModSetupList);
+            this.Controls.Add(this.buttonModDisable);
+            this.Controls.Add(this.buttonModEnable);
+            this.Controls.Add(this.listboxModsEnabled);
+            this.Controls.Add(this.listboxModsAvailable);
+            this.Name = "Form1";
+            this.Text = "Zomboid Server - Mod Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listboxModsAvailable;
+        private System.Windows.Forms.ListBox listboxModsEnabled;
+        private System.Windows.Forms.Button buttonModEnable;
+        private System.Windows.Forms.Button buttonModDisable;
+        private System.Windows.Forms.Button buttonModSetupList;
+        private System.Windows.Forms.ListBox listboxModSelectedPreview;
+        private System.Windows.Forms.Label labelOne;
+        private System.Windows.Forms.RichTextBox richtextGeneratedConfig;
+    }
+}
+
---
+++ ZomboidServerModManager/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZomboidServerModManager
+{
+    public partial class Form1 : Form
+    {
+        public CModManager ModManager = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void buttonModEnable_Click(object sender, EventArgs e)
+        {
+            string strSelected = listboxModsAvailable.SelectedItem.ToString();
+            ModManager.SetEnabled(strSelected, true);
+            UpdateAvailableMods();
+        }
+
+        private void buttonModDisable_Click(object sender, EventArgs e)
+        {
+            string strSelected = listboxModsEnabled.SelectedItem.ToString();
+            ModManager.SetEnabled(strSelected, false);
+            UpdateAvailableMods();
+        }
+
+        private void listboxModsAvailable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listboxModsAvailable.SelectedItem == null)
+                return;
+
+            listboxModSelectedPreview.Items.Clear();
+            if (listboxModsAvailable.SelectedItem.ToString().Count() == 0)
+                return;
+
+            CModItem mod = ModManager.GetModByName(listboxModsAvailable.SelectedItem.ToString());
+            listboxModSelectedPreview.Items.Add("Workshop ID: " + mod.workshopId);
+            foreach( string s in mod.modIDs)
+            {
+                if( s.Count() > 0)
+                {
+                    listboxModSelectedPreview.Items.Add("Mod ID: " + s);
+                }
+            }
+        }
+
+        private void UpdateAvailableMods()
+        {
+            //clear both lists
+            listboxModsAvailable.Items.Clear();
+            listboxModsEnabled.Items.Clear();
+
+            //disabled
+            foreach (CModItem etx in ModManager.modListDisabled)
+            {
+                if (etx.name.Count() == 0)
+                    continue;
+
+                listboxModsAvailable.Items.Add(etx.name);
+            }
+
+            //enabled
+            foreach ( CModItem etx in ModManager.modListEnabled)
+            {
+                if (etx.name.Count() == 0)
+                    continue;
+
+                listboxModsEnabled.Items.Add(etx.name);
+            }
+        }
+
+        private void buttonModSetupList_Click(object sender, EventArgs e)
+        {
+            string configWorkshopID = "WorkshopItems=";
+            string configModIDs = "Mods=";
+
+            foreach ( CModItem mod in ModManager.modListEnabled)
+            {
+                //workshopID
+                configWorkshopID += (mod.workshopId + ";");
+
+                //mod IDs
+                foreach( string s in mod.modIDs)
+                {
+                    configModIDs += (s + ";");
+                }
+            }
+
+            richtextGeneratedConfig.Text = configWorkshopID + "\n" + configModIDs;
+
+            List<string> listStringConfig = File.ReadAllLines("pzserver.ini").ToList();
+            if (listStringConfig.Count() == 0)
+                return;
+
+            for (int i = 0; i != listStringConfig.Count(); i++)
+            {
+                string strCurrent = listStringConfig[i];
+                if (strCurrent.Count() == 0)
+                    continue;
+
+                if (strCurrent.Contains("Mods="))
+                {
+                    listStringConfig[i] = configModIDs;
+                    continue;
+                }
+
+                if (strCurrent.Contains("WorkshopItems="))
+                {
+                    listStringConfig[i] = configWorkshopID;
+                    continue;
+                }
+            }
+
+            File.WriteAllLines("pzserver.ini", listStringConfig);
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            ModManager = new CModManager(files[0]);
+            if (!ModManager.isSetup)
+            {
+                MessageBox.Show("Failed to load mod file");
+                return;
+            }
+
+            //add items to list
+            UpdateAvailableMods();
+        }
+    }
+
+    public class CModItem
+    {
+        public int index = 0;
+        public string name = "invalid";
+        public string workshopId = "";
+        public List<string> modIDs = new List<string>();
+        public bool isSetup = false;
+        public CModItem(string inputName)
+        {
+            name = inputName;
+
+            if (!Init())
+                return;
+            isSetup = true;
+        }
+        private bool Init()
+        {
+            return false;
+        }
+    }
+
+    public class CModManager
+    {
+        string modFilePath = "INVALID";
+        List<string> modFile = new List<string>();
+        private List<CModItem> modList = new List<CModItem>();
+        public List<CModItem> modListDisabled = new List<CModItem>();
+        public List<CModItem> modListEnabled = new List<CModItem>();
+        public bool isSetup = false;
+
+        public CModManager()
+        {
+            MessageBox.Show("Empty setup");
+        }
+
+        public CModManager(string modListFile)
+        {
+            if (modListFile == null)
+                return;
+
+            modFilePath = modListFile;
+            if (!Init())
+                return;
+
+            isSetup = true;
+        }
+
+        public void SetEnabled(string modName, bool newEnabled)
+        {
+            CModItem mod = GetModByName(modName, !newEnabled);
+            if (mod == null)
+                return;
+
+            //remove from both
+            modListDisabled.Remove(mod);
+            modListEnabled.Remove(mod);
+
+            //add to selected
+            if( newEnabled)
+            {
+                modListEnabled.Add(mod);
+            }
+            else
+            {
+                modListDisabled.Add(mod);
+            }
+        }
+
+        public CModItem GetModByName(string name, bool itemsEnabled = false)
+        {
+            if( !itemsEnabled)
+            {
+                foreach (CModItem etx in modListDisabled)
+                {
+                    if (etx.name == name)
+                        return etx;
+                }
+            } else
+            {
+                foreach (CModItem etx in modListEnabled)
+                {
+                    if (etx.name == name)
+                        return etx;
+                }
+            }
+            
+            
+
+            return null;
+        }
+
+        private bool Init()
+        {
+            List<string> modFile = File.ReadAllLines(modFilePath).ToList();
+            if (modFile.Count() == 0)
+                return false;
+
+            for( int i =0; i!= modFile.Count(); i++)
+            {                
+                string sCurrent = modFile[i];
+
+                //skip empty lines
+                if (sCurrent.Count() == 0)
+                    continue;
+
+                //new mod table
+                if (sCurrent.StartsWith("-"))
+                {
+                    CModItem mod = new CModItem(sCurrent.Substring(1, sCurrent.Count() - 1));
+                    modList.Add(mod);
+                    continue;
+                }
+                
+                //add workshop ID to last item
+                else if( sCurrent.StartsWith("Workshop ID: "))
+                {
+                    modList.Last().workshopId = sCurrent.Substring(12).Trim();
+                    continue;
+                }
+
+                //add mod IDs to last item
+                else if (sCurrent.StartsWith("Mod ID: "))
+                {
+                    modList.Last().modIDs.Add(sCurrent.Substring(8).Trim());
+                    continue;
+                }
+            }
+
+            //set temp list
+            modListDisabled = modList;
+
+            return true;
+        }
+    }
+}
